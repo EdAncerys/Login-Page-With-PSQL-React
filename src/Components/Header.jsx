cd8498b0d7e4ce62403539b5bbd791d193ff3a48
Ed <lookatemail@gmail.com>
2020-09-12 14:21:27 +0100

    Add header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import colors from '../config/colors';
 
 export default function Header({ props }) {
   return (
     <div style={styles.container}>
-      <div>header</div>
+      <div style={styles.logo}>Login Page</div>
     </div>
   );
 }
 
 const styles = {
   container: {
+    display: 'grid',
+    alignItems: 'center',
     height: 100,
+    paddingLeft: '10vw',
+    backgroundColor: colors.primary,
+  },
+  logo: {
+    color: colors.white,
+    fontSize: 28,
   },
 };
