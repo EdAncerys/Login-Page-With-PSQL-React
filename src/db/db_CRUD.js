ca143c1dba8711293c70c1bf43728e78e00d1d39
Ed <lookatemail@gmail.com>
2020-09-14 11:37:14 +0100

    Able to conect and get data from db

---
+++ src/db/db_CRUD.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db_conection');
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+//create a todo
+
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all todos
+
+app.get('/users', async (req, res) => {
+  try {
+    const allUsers = await pool.query('SELECT * FROM users');
+    res.json(allUsers.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('server has started on port 5000');
+});
